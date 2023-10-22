d68452ed2bf3f755e566bc896bc61cb96f479edb
Roman Yudin <yudiol114@gmail.com>
2023-10-22 22:22:12 +0300

    feat: добавить UserDto

--- src/main/java/com/example/mogoDB/dto/UserDto.java
+++ src/main/java/com/example/mogoDB/dto/UserDto.java
@@ -1,10 +1,10 @@
-package com.example.mogoDB.model;
+package com.example.mogoDB.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Setter
@@ -12,11 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "users")
-public class User {
-    @Id
-    private String id;
+public class UserDto {
+
+    @Schema(description = "имя", example = "Иван")
     private String name;
+    @Schema(description = "фамилия", example = "Иванов")
     private String surname;
+    @Schema(description = "возраст", example = "35")
     private int age;
-    private String createdAt;
 }
